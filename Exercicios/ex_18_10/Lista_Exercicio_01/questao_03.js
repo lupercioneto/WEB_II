b750fe34bb2a10d7b350ac5cc9f91d0f0acde3d5
Lupercio Neto <lupercionetof@gmail.com>
2025-10-21 13:03:59 -0300

    Atividade 01: JS e Node Básico - Finalizada

--- Exercicios/ex_18_10/Lista_Exercicio_01/questao_03.js
+++ Exercicios/ex_18_10/Lista_Exercicio_01/questao_03.js
@@ -58,7 +58,49 @@ console.log(factorial(5));
 // Alternativa D
 
 function findOdds(numArrays) {
+    let odds = []
+
     array.forEach(num => {
-        num 
+        if (num % 2 != 0) {
+            odds.push(num)
+        } 
     });
+
+    console.log(`Os números ímpares desse array são: ${odds}`)
+}
+
+findOdds([2,12,34,67,99,111,341])
+
+// Alternativa E
+
+function calcPrice(price, qtt, sale = 0) {
+    let totalPrice = (price * qtt) - sale 
+
+    return totalPrice
+
+}
+
+console.log(`O preço total do(s) produto(s) é : R$${totalPrice.toFixed(2)}`)
+
+// Alternativa F
+
+const contaBancaria = {
+    saldo: 1000,
+    accountNum: "12345",
+    deposit: function(depCash) {
+        this.saldo += depCash
+        
+    },
+
+    sacar: function(debtCash) {
+        if (debtCash > this.saldo) {
+            throw new Error("ERROR: Saldo insuficiente");
+            
+        }
+    },
+
+    showSaldo: function() {
+        console.log(`Seu saldo atual é: R$${this.saldo.toFixed(2)}`)   
+    }
+
 }
